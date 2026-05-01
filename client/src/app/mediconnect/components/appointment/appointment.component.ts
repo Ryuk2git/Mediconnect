c9b39e455ab1b7e3a49d9ccabfa107ce0b3cf407
Ryuk2git <dummy@gmail.com>
2026-05-01 20:24:27 +0000

    Day 20

--- client/src/app/mediconnect/components/appointment/appointment.component.ts
+++ client/src/app/mediconnect/components/appointment/appointment.component.ts
@@ -1,4 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-export class AppointmentComponent {
+@Component({
+  selector: 'app-appointment',
+  templateUrl: './appointment.component.html',
+  styleUrls: ['./appointment.component.scss']
+})
+export class AppointmentCreateComponent implements OnInit {
+  appointmentForm!: FormGroup;
+  successMessage: string | null = null;
+  errorMessage: string | null = null;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.appointmentForm = this.fb.group({
+      appointmentId: ['', [Validators.required, Validators.min(1)]],
+      patientId: ['', [Validators.required, Validators.min(1)]],
+      clinicId: ['', [Validators.required, Validators.min(1)]],
+      appointmentDate: ['', [Validators.required]],
+      status: ['', [Validators.required]],
+      purpose: ['', [Validators.required, Validators.minLength(5)]]
+    });
+  }
+
+  onSubmit(): void {
+    this.successMessage = null;
+    this.errorMessage = null;
+
+    if (this.appointmentForm.valid) {
+      this.successMessage = 'Appointment created successfully!';
+      console.log('Form Data:', this.appointmentForm.value);
+    } else {
+      this.errorMessage = 'Please fill in all required fields correctly.';
+      this.appointmentForm.markAllAsTouched();
+    }
+  }
+
+  resetForm(): void {
+    this.appointmentForm.reset({
+            appointmentId: null,
+            patientId: null,
+            clinicId: null,
+            appointmentDate: '',
+            status: '',
+            purpose: ''
+        });
+    this.successMessage = null;
+    this.errorMessage = null;
+  }
 }
--- client/src/app/mediconnect/mediconnect-routing.module.ts
+++ client/src/app/mediconnect/mediconnect-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AppointmentCreateComponent } from "./components/appointment/appointment.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '/appointment', component: AppointmentCreateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
